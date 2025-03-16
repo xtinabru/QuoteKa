16eb13c89ead54eb7bc2be327240aedef6578dca
Cristofor4ik <xtinabruscaginxxx@gmail.com>
2025-03-16 21:54:24 +0200

    refactor: Clean up and refactor code

--- app/src/main/java/com/example/quotekaapplication/data/models/Quote.kt
+++ app/src/main/java/com/example/quotekaapplication/data/models/Quote.kt
@@ -1,13 +1,12 @@
 package com.example.quotekaapplication.data.models
 
 data class Quote(
-    val quote: String, // Текст цитаты
-    val author: String, // Автор цитаты
-    val category: String // Категория цитаты
+    val quote: String,
+    val author: String,
+    val category: String
 ) {
-    // Переопределение equals и hashCode для корректного сравнения цитат
+
     override fun equals(other: Any?): Boolean {
-        // Проверяем, что другой объект является экземпляром Quote и сравниваем все поля
         return other is Quote &&
                 other.quote == this.quote &&
                 other.author == this.author &&
@@ -15,7 +14,6 @@ data class Quote(
     }
 
     override fun hashCode(): Int {
-        // Генерируем hashCode на основе всех полей
         return quote.hashCode() * 31 + author.hashCode() * 31 + category.hashCode()
     }
 }
